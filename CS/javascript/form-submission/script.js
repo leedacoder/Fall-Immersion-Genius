fba575040c5700fa0647419b319ae8c0aee1a675
leedacoder <kylee.jones-mckinnon@hgs.hiddengeniusproject.org>
2025-11-25 11:16:07 -0500

    Update formSubmission function for event handling
    Fix form submission event listener and element retrieval.

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,11 @@
 function formSubmission() {
-    // Your code here.
+    // function formSubmission() {
+    // document.addEventListener("DOMContentLoaded")
+    document.getElementById.myForm
+    const myFormElement = document.getElementById("myForm");
+    myForm.addEventListener('submit', function(event)
+    document.getNameById("myForm")
+    document.getEmailById("myForm")
+    document.getMessageById("myForm")
 }
+
